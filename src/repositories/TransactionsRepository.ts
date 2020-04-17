275e24bbb778f57ad62d0bb7d689b89757e6b42c
Michel dos Santos Kuguio <michel.kuguio@luizalabs.com>
2020-04-16 22:32:05 -0300

    desafio finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,14 +15,39 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let data: Balance = { income: 0, outcome: 0, total: 0 };
+    if (this.transactions.length > 0)
+      data = this.transactions
+        .map(transaction => {
+          const balance: Balance = {
+            income: 0,
+            outcome: 0,
+            total: 0,
+          };
+          if (transaction.type === 'income') {
+            balance.income = transaction.value;
+          } else {
+            balance.outcome = transaction.value;
+          }
+          return balance;
+        })
+        .reduce((accumulator, currentValue) => {
+          accumulator.income += currentValue.income;
+          accumulator.outcome += currentValue.outcome;
+          accumulator.total = accumulator.income - accumulator.outcome;
+          return accumulator;
+        });
+    return data;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ type, value, title }: Omit<Transaction, 'id'>): Transaction {
+    const transaction = new Transaction({ type, value, title });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Omit<Transaction, 'id'>): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && balance.total < value) {
+      throw Error('insufficient Funds');
+    }
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
